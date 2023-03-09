fed08db26bffa4daf78d2a82c73020f3b7f9656d
Elhexiar <miriel.mathis@gmail.com>
2023-03-09 18:01:02 +0100

    yep

---
+++ Ressources/temp tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="temp tileset" tilewidth="64" tileheight="64" tilecount="2500" columns="50">
+ <image source="temp tileset.jpg" width="3200" height="3200"/>
+</tileset>
