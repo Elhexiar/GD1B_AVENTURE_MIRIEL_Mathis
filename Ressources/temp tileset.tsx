0ee33bccdb18c2673e6dbcc67d8412812edb0632
Elhexiar <miriel.mathis@gmail.com>
2023-03-16 16:05:13 +0100

    ouai ouai ouai

--- Ressources/temp tileset.tsx
+++ Ressources/temp tileset.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="temp tileset" tilewidth="64" tileheight="64" tilecount="2500" columns="50">
- <image source="temp tileset.jpg" width="3200" height="3200"/>
+ <image source="temp tileset.png" width="3200" height="3200"/>
+ <tile id="0">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="estSolide" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
